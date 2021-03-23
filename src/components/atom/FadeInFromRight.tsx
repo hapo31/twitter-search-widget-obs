788e73cce0f8f52b16f29c3e94f0aec3af3735fd
happou31 <happo31@gmail.com>
2021-03-24 00:23:49 +0900

    feat: ディレイつけれるようにした

--- src/components/atom/FadeInFromRight.tsx
+++ src/components/atom/FadeInFromRight.tsx
@@ -3,30 +3,44 @@ import styled from "styled-components";
 
 type Props = {
   time: number;
+  delay: number;
+  play: boolean;
+  onAnimationEnd?: () => void;
   style?: CSSProperties;
   children?: React.ReactNode;
 };
 
 export const FadeInFromRight = (props: Props) => (
-  <FadeInContainer t={props.time} style={props.style}>
+  <FadeInContainer
+    className={props.play ? "anim" : ""}
+    t={props.time}
+    d={props.delay}
+    style={props.style}
+    onAnimationEnd={props.onAnimationEnd}
+  >
     {props.children}
   </FadeInContainer>
 );
 
+type FadeInContainerProps = {
+  t: number;
+  d: number;
+};
+
 const FadeInContainer = styled.div`
   overflow: hidden;
   white-space: nowrap;
-  animation: move ${({ t }: { t: number }) => `${t}`}s ease 0s 1;
+  &.anim {
+    animation: move ${({ t }: FadeInContainerProps) => `${t}`}s ease ${({ d }: FadeInContainerProps) => `${d}`}s 1;
 
-  @keyframes move {
-    0% {
-      position: absolute;
-      margin-left: 100%;
-    }
+    @keyframes move {
+      0% {
+        margin-left: 100%;
+      }
 
-    100% {
-      position: inherit;
-      margin-left: 0;
+      100% {
+        margin-left: 0;
+      }
     }
   }
 `;
