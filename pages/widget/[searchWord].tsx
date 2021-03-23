ecbd9658976be102807c4ade3a9af05e0140331f
happou31 <happo31@gmail.com>
2021-03-24 00:23:23 +0900

    fix: 分離

--- pages/widget/[searchWord].tsx
+++ pages/widget/[searchWord].tsx
@@ -1,14 +1,15 @@
 import { GetServerSideProps } from "next";
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
 import { appAuthThunk, searchTweetsThunk, storeToken } from "../../src/actions/twitter";
-import { FadeInFromRight } from "../../src/components/atom/FadeInFromRight";
+import { TwitterSearch } from "../../src/components/organisms/TwitterSearch";
+import { ERR_REQUIRE_TOKEN } from "../../src/constants/error";
 import { RootState } from "../../src/store/root";
 
 type Props = {
   searchWord?: string;
   token?: string;
+  transition: number;
 };
 
 export default function TwitterHashtag(props: Props) {
@@ -20,14 +21,22 @@ export default function TwitterHashtag(props: Props) {
   const { token, tweets } = twitter;
 
   const [currentTweet, setCurrentTweet] = useState(0);
+  const [error, setError] = useState(0);
   const timerRef = useRef(0);
 
   useEffect(() => {
-    if (timerRef.current === 0) {
-      timerRef.current = window.setInterval(() => {
-        setCurrentTweet((currentTweet) => currentTweet + 1);
-      }, 10000);
+    if (timerRef.current !== 0) {
+      window.clearInterval(timerRef.current);
     }
+    timerRef.current = window.setInterval(() => {
+      setCurrentTweet((currentTweet) => (currentTweet = (currentTweet + 1) % tweets.length));
+    }, 10000);
+
+    return () => {
+      if (timerRef.current !== 0) {
+        window.clearInterval(timerRef.current);
+      }
+    };
   }, [tweets]);
 
   useEffect(() => {
@@ -38,7 +47,7 @@ export default function TwitterHashtag(props: Props) {
       } else if (props.token != null) {
         dispatch(storeToken(props.token));
       } else {
-        dispatch(appAuthThunk());
+        setError(ERR_REQUIRE_TOKEN);
       }
     } else if (props.searchWord) {
       localStorage.setItem("token", token);
@@ -46,119 +55,25 @@ export default function TwitterHashtag(props: Props) {
     }
   }, [token]);
 
-  return (
-    <>
-      <Container>
-        <LogoContainer>
-          <img src="../../images/twitter-white.png" alt="" />
-        </LogoContainer>
-        <ContentContainer>
-          <Tweet>
-            {tweets.length >= 1
-              ? tweets
-                  .filter((tweet, i) => i === currentTweet % tweets.length)
-                  .map((tweet) => (
-                    <FadeInFromRight key={`tweet-${tweet.id}`} time={2}>
-                      <div>{tweet.text.replaceAll(/#[^\s]+\s?/g, "")}</div>
-                      <Author>
-                        by{" "}
-                        <Name>
-                          {tweet.name} @{tweet.screenName}
-                        </Name>
-                      </Author>
-                    </FadeInFromRight>
-                  ))
-              : null}
-          </Tweet>
-          <HashTag>#{props.searchWord}</HashTag>
-        </ContentContainer>
-      </Container>
-    </>
-  );
+  useEffect(() => {
+    const html = document.getElementsByTagName("html");
+    html[0].style.overflow = "hidden";
+  }, []);
+
+  return tweets[currentTweet] ? (
+    <TwitterSearch searchWord={props.searchWord ?? ""} tweet={tweets[currentTweet]} fadeInTimeSec={props.transition} />
+  ) : null;
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
   const searchWord = context.query["searchWord"] as string;
   const token = context.params?.["token"] as string;
+  const transition = context.params?.["token"] as string;
   return {
     props: {
       searchWord: searchWord ?? null,
       token: token ?? null,
+      transition: transition ? parseInt(transition) : 2,
     },
   };
 };
-
-const Container = styled.main`
-  background-color: white;
-  width: 98vw;
-  height: 200px;
-  border: solid 3px #1d98f0;
-  border-top-left-radius: 90px;
-
-  display: flex;
-  justify-content: flex-start;
-
-  overflow: hidden;
-`;
-
-const LogoContainer = styled.div`
-  height: 105%;
-  width: 200px;
-  min-width: 200px;
-  margin: -2px;
-  border-radius: 90px;
-  border-bottom-left-radius: 0;
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-  border-right: solid 2px #0880c0;
-  background-color: #1d98f0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  > img {
-    width: 60%;
-    height: 60%;
-    object-fit: scale-down;
-    display: block;
-  }
-`;
-
-const ContentContainer = styled.div`
-  height: 100%;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-`;
-
-const Tweet = styled.div`
-  padding: 10px;
-  font-size: 48px;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  height: 70%;
-`;
-
-const HashTag = styled.div`
-  border: solid 2px #0880c0;
-  height: 100%;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 48px;
-  flex: 3;
-  background-color: #1d98f0;
-  white-space: nowrap;
-  color: #efefef;
-`;
-
-const Author = styled.div`
-  font-size: 16px;
-`;
-
-const Name = styled.span`
-  font-size: 20px;
-  font-weight: bold;
-  color: #1d98f0;
-`;
---
+++ src/components/organisms/TwitterSearch.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Tweet } from "../../store/twitter";
+import { FadeInFromRight } from "../atom/FadeInFromRight";
+
+type Props = {
+  searchWord: string;
+  tweet: Tweet;
+  fadeInTimeSec: number;
+};
+
+export const TwitterSearch = (props: Props) => {
+  const { tweet, searchWord, fadeInTimeSec } = props;
+  const [isPlay, setIsPlay] = useState(false);
+  const [showAuthor, setShowAuthor] = useState(false);
+
+  useEffect(() => {
+    setShowAuthor(false);
+    setIsPlay(true);
+    setTimeout(() => {
+      setShowAuthor(true);
+    }, 500);
+    setTimeout(() => {
+      setIsPlay(false);
+    }, fadeInTimeSec * 1000);
+  }, [tweet]);
+
+  return (
+    <>
+      <Container>
+        <LogoContainer>
+          <img src="../../images/twitter-white.png" alt="" />
+        </LogoContainer>
+        <ContentContainer>
+          <TweetContainer>
+            <FadeInFromRight play={isPlay} time={fadeInTimeSec} delay={0}>
+              <div>{tweet.text.replace(/#[^\s]+\s?/g, "")}</div>
+              {showAuthor ? (
+                <FadeInFromRight play={true} time={fadeInTimeSec} delay={0}>
+                  <Author>
+                    by{" "}
+                    <Name>
+                      {tweet.name} @{tweet.screenName}
+                    </Name>
+                  </Author>
+                </FadeInFromRight>
+              ) : null}
+            </FadeInFromRight>
+          </TweetContainer>
+          <HashTag>#{searchWord}</HashTag>
+        </ContentContainer>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.main`
+  background-color: white;
+  width: 98vw;
+  height: 200px;
+  border: solid 3px #1d98f0;
+  border-top-left-radius: 90px;
+
+  display: flex;
+  justify-content: flex-start;
+
+  overflow: hidden;
+`;
+
+const LogoContainer = styled.div`
+  height: 105%;
+  width: 200px;
+  min-width: 200px;
+  margin: -2px;
+  border-radius: 90px;
+  border-bottom-left-radius: 0;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  border-right: solid 2px #0880c0;
+  background-color: #1d98f0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > img {
+    width: 60%;
+    height: 60%;
+    object-fit: scale-down;
+    display: block;
+  }
+`;
+
+const ContentContainer = styled.div`
+  height: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
+
+const TweetContainer = styled.div`
+  padding: 10px;
+  font-size: 48px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  height: 70%;
+`;
+
+const HashTag = styled.div`
+  border: solid 2px #0880c0;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 48px;
+  flex: 3;
+  background-color: #1d98f0;
+  white-space: nowrap;
+  color: #efefef;
+`;
+
+const Author = styled.div`
+  font-size: 16px;
+`;
+
+const Name = styled.span`
+  font-size: 20px;
+  font-weight: bold;
+  color: #1d98f0;
+`;
